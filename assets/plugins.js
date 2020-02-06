5b2d314cf380079ec48c4c26af828215ddf0ab19
Peter Mikola <mikopet@gmail.com>
2020-02-06 09:59:39 +0100

    actualizing the data, and some facelift

--- assets/plugins.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.barres li').each(function() {
-  	var pourcentage = $(this).attr('data-skills');
-  	$(this).append($("<span></span>").animate({
-  		width : ''+ pourcentage +'%' }, 2000));	
-  });
-});
